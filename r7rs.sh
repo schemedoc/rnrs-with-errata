6a62eeaf8f52f7061b16f42dc90fd6823555c3bd
Lassi Kortela <lassi@lassi.io>
2019-07-01 15:25:23 +0300

    Refactor r7rs.sh

--- r7rs.sh
+++ r7rs.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
+fetch="curl --fail --silent --show-error -o"
 set -eux
 
 # These files are sourced from John Cowan's personal Bitbucket repos.
 # We shall coordinate with him to ensure we have up-to-date versions.
 
-curl --fail --silent --show-error -o r7rs-small-official.pdf \
+$fetch r7rs-small-official.pdf \
 	https://bitbucket.org/cowan/r7rs/raw/errata/rnrs/r7rs-official.pdf
 
-curl --fail --silent --show-error -o r7rs-small-errata.md \
+$fetch r7rs-small-errata.md \
 	https://bitbucket.org/cowan/r7rs-wg1-infra/raw/default/R7RSSmallErrata.md
 
-curl --fail --silent --show-error -o r7rs-small-errata-corrected.pdf \
+$fetch r7rs-small-errata-corrected.pdf \
 	https://bitbucket.org/cowan/r7rs/raw/errata/rnrs/r7rs.pdf
