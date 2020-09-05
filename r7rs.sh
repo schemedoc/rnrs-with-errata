ff499a7cde1401e0c6bf3625c67142b39437b8c1
Lassi Kortela <lassi@lassi.io>
2020-09-05 19:32:52 +0300

    Fix downloading r7rs from GitHub

--- r7rs.sh
+++ r7rs.sh
@@ -8,10 +8,10 @@ set -eux
 # We shall coordinate with him to ensure we have up-to-date versions.
 
 $fetch r7rs.pdf \
-    https://github.com/johnwcowan/r7rs-spec/raw/errata/rnrs/r7rs-official.pdf
+    https://raw.githubusercontent.com/johnwcowan/r7rs-spec/errata/rnrs/r7rs-official.pdf
 
 $fetch r7rs-errata-corrected.pdf \
-    https://github.com/johnwcowan/r7rs-spec/raw/errata/rnrs/r7rs.pdf
+    https://raw.githubusercontent.com/johnwcowan/r7rs-spec/errata/rnrs/r7rs.pdf
 
 $fetch r7rs-errata.md \
     https://raw.githubusercontent.com/johnwcowan/r7rs-work/master/R7RSSmallErrata.md
